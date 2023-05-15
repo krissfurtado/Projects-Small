1083ddada38e34b732321d94a6a77d6994f6d7a2
krissfurtado <krissfurtado@gmail.com>
2023-05-14 21:51:19 -0300

    set width and hieght to canvas element and context

--- game-PingPong/script.js
+++ game-PingPong/script.js
@@ -2,9 +2,13 @@ const canvasEl = document.querySelector("canvas"),
 canvasCtx = canvasEl.getContext("2d")
 
 function setup() {
+    canvasEl.width = window.innerWidth
+    canvasCtx.width = window.innerWidth
 
+    canvasEl.height = window.innerHeight
+    canvasCtx.height = window.innerHeight
 }
 
 function draw() {
-    
+
 }
