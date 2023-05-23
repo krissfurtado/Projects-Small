79c8aa4f512ebaa00e5ccc82d3e9648a93f06fbd
krissfurtado <krissfurtado@gmail.com>
2023-05-22 22:50:03 -0300

    Creating a center line

--- game-PingPong/script.js
+++ game-PingPong/script.js
@@ -1,5 +1,6 @@
 const canvasEl = document.querySelector("canvas"),
 canvasCtx = canvasEl.getContext("2d")
+const lineWidth = 15
 
 function setup() {
     canvasEl.width = canvasCtx.width = window.innerWidth
@@ -11,6 +12,11 @@ function draw() {
     canvasCtx.fillStyle = "#286047"
     //drawing a rectangle, starting from the top left corner
     canvasCtx.fillRect(0, 0, window.innerWidth, window.innerHeight)
+
+    //now the rectangle color is white
+    canvasCtx.fillStyle = "#ffffff"
+    canvasCtx.fillRect(window.innerWidth/2-lineWidth/2, 0, lineWidth, window.innerHeight)
+
 }
 
 setup()
